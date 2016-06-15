29b683803bd5ef9e031519e11b8a06b4c2d662a4
yann <xxx>
2016-06-15 10:31:34 +0800

    fx

--- authorization/__init__.py
+++ authorization/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPTokenAuth, HTTPBasicAuth, MultiAuth
 import os
 
-app = Flask(__name__, template_folder="../templates")
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
 app.debug = True
 app.secret_key = 'secret'
 
